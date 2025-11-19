d3b0ecbc8441abf63601b854de1207fccafc6a79
Marcos250800 <elkchy25@gmail.com>
2025-11-19 23:45:50 +0000

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   checkInterval: 5 * 60 * 1000, // 5 minutos
-  base44ApiUrl: "TU_ENDPOINT_DE_CITAS", // Esto lo dejamos tal cual por ahora
+  base44ApiUrl: "https://www.citaconsular.es/es/hosteds/widgetdefault/25b6cfa9f112aef4ca19457abc237f7ba", // Esto lo dejamos tal cual por ahora
   apiKey: "TU_API_KEY_AQUI"            // Esto lo dejamos tal cual por ahora
 };
+
